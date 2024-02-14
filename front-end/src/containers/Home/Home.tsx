ecf3fd268843617830063abedbc5057b73a3298b
sachie5 <sachamccormack@gmail.com>
2024-02-14 14:59:20 +0000

    designed homepage

--- front-end/src/containers/Home/Home.tsx
+++ front-end/src/containers/Home/Home.tsx
@@ -7,8 +7,8 @@ type HomeProps = {
 const Home = ({}: HomeProps) => {
     return (
         <section className="home">
-            <h1>Welcome to The Anime Directory where you can keep track of all the anime you have watched.</h1>
-            <h1>The directory starts with 9 anime series curated by the creator, Sacha, for you to try.</h1>
+            <h1 className="home__intro home__intro--one">Welcome to The Anime Directory where you can keep track of all the anime you have watched.</h1>
+            <h1 className="home__intro home__intro--two">The directory starts with nine anime series curated by the creator, Sacha, for you to try.</h1>
         </section>
     )
 }
